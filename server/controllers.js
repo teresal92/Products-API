a1aa073de4b4621c32223c7e2056f6254c47be32
Teresa Lew <teresalew92@gmail.com>
2022-03-03 14:01:43 -0800

    updated getStyles query to include photos

--- server/controllers.js
+++ server/controllers.js
@@ -17,8 +17,8 @@ module.exports = {
   getProductInfo: async (req, res) => {
     const { productId } = req.params;
     try {
-      const products = await pool.query("SELECT *, (SELECT json_agg(json_build_object('feature', feature, 'value', value)) AS features \
-                                         FROM features where product_id = $1) FROM products WHERE id = $1;", [productId]);
+      const products = await pool.query('SELECT *, (SELECT json_agg(json_build_object("feature", feature, "value", value)) AS features \
+                                         FROM features where product_id = $1) FROM products WHERE style_id = $1;', [productId]);
       res.json(products.rows[0]);
     } catch (err) {
       res.status(404).send(`Error retrieving product list: ${err.message}`);
@@ -27,10 +27,13 @@ module.exports = {
 
   getStyles: async (req, res) => {
     const { productId } = req.params;
-    console.log(productId);
-
     try {
-      const products = await pool.query('SELECT * FROM styles WHERE productId = $1;', [productId]);
+      const products = await pool.query("SELECT styles.productId AS style_id, styles.name, styles.original_price, styles.sale_price, styles.default_style AS default, \
+                                                json_agg(json_build_object('thumbnail_url', photos.thumbnail_url, 'url', photos.url)) AS photos \
+                                        FROM styles \
+                                        INNER JOIN photos ON styles.id = photos.styleId \
+                                        WHERE styles.productId = $1 \
+                                        GROUP BY styles.productId, styles.name, styles.original_price, styles.sale_price, styles.default_style;", [productId]);
       res.json(products.rows[0]);
     } catch (err) {
       console.error(err);
@@ -38,6 +41,7 @@ module.exports = {
     }
   },
 
+  // select json_build_object('thumbnail_url', thumbnail_url, 'url', url) AS photos FROM photos WHERE styleid = 1;
   getRelated: (req, res) => {
     const { productId } = req.params;
     // axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${productId}/related`, {
