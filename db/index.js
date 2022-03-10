10623ad6f32c9793efe1ddb16b5f2430adbd0fe7
Teresa Lew <teresalew92@gmail.com>
2022-03-09 17:49:18 -0800

    fixed error with new dotenv variables

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 const pool = new Pool({
   host: process.env.DB_HOST,
-  user: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE
